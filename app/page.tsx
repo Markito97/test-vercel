328a6d40dc4e3cc3744e6241304f66c5343f0ab8
sorokin mark <popov.a@bit76.ru>
2025-02-23 22:17:44 +0300

    --wip change main title

--- app/page.tsx
+++ app/page.tsx
@@ -33,18 +33,12 @@ export default function Home() {
                   <div className="relative z-10 lg:h-auto pt-[90px] lg:pt-[90px] lg:min-h-[300px] flex flex-col items-center justify-center sm:mx-auto md:w-3/4 lg:mx-0 lg:w-full gap-4 lg:gap-8">
                     <div className="flex flex-col items-center">
                       <h1
-                        className={`${comicNeue.className} text-foreground text-4xl sm:text-5xl sm:leading-none lg:text-7xl`}
+                        className={`${comicNeue.className} text-4xl whitespace-nowrap sm:text-5xl sm:leading-none lg:text-5xl`}
                       >
-                        <span className="block text-foreground">
-                          Энергобит
-                        </span>
-                        <span className="text-brand block md:ml-0">
-                          Умное управление энергозатратами
-                        </span>
+                          Энергобит - ключ к энергоэффективности
                       </h1>
                       <p className={`${roboto.className} pt-2 text-foreground my-3 text-sm sm:mt-5 lg:mb-0 sm:text-base lg:text-lg`}>
-                        Ваш надежный инструмент для оптимизации расходов на электроэнергию.
-                        <br /> Благодаря передовым алгоритмам на базе искусственного интеллекта
+                        Решения на базе искусственного интеллекта для управления энергозатратами. <br /> Снижайте расходы на электроэнергию, повышайте эффективность и сохраняйте ресурсы без лишних усилий.
                       </p>
                     </div>
                     <div className="flex items-center gap-2">
@@ -65,7 +59,7 @@ export default function Home() {
         <WhatYouGetList />
         <PossibilitiesList />
         <ReviewsList />
-        <ContactUs/>
+        <ContactUs />
       </main>
     </div>
   );
--- components/ui/hero-marquee.tsx
+++ components/ui/hero-marquee.tsx
@@ -2,32 +2,21 @@ import React from "react"
 import { FasieIcon } from "../icons/fasie-icon"
 import { RospatentIcon } from "../icons/rospatent-icon"
 import { RegisterIcon } from "../icons/register-icon"
-
-
+import Marquee from "react-fast-marquee";
 
 export const HeroMarquee = () => {
-    return <div className="pb-14 md:pb-24">
-        <div className="max-w-md md:max-w-lg lg:max-w-2xl mx-auto overflow-x-hidden">
-            <div className="relative w-full opacity-90 dark:opacity-70">
-                <div className="relative flex overflow-x-hidden">
-                    <div className="flex items-center gap-8 py-12 animate-marquee whitespace-nowrap">
-                        {[...Array(2)].map((_, i) => (
-                            <React.Fragment key={i}>
-                                <div className="flex-shrink-0 min-w-[100px]">
-                                    <FasieIcon />
-                                </div>
-                                <div className="flex-shrink-0 min-w-[100px]">
-                                    <RospatentIcon />
-                                </div>
-                                <div className="flex-shrink-0 min-w-[100px]">
-                                    <RegisterIcon />
-                                </div>
-                            </React.Fragment>
-                        ))}
-                    </div>
-                </div>
+    return (
+        <div className="pb-14 md:pb-24">
+            <div className="max-w-md md:max-w-lg lg:max-w-2xl mx-auto">
+                <Marquee speed={50} gradient={false}>
+                    <div className="min-w-[100px]"><FasieIcon /></div>
+                    <div className="min-w-[100px]"><RospatentIcon /></div>
+                    <div className="min-w-[100px]"><RegisterIcon /></div>
+                </Marquee>
             </div>
+            <p className="w-full text-center text-sm text-foreground-lighter mt-6 lg:mt-8">
+                    Программа ЭнергоБИТ включена в реестр российского ПО (реестровая запись № 23968 от 06.09.2024г.)
+                </p>
         </div>
-    </div>
-
+    );
 }
